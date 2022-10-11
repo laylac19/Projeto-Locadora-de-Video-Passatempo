3ec74417c8f64f0c81b628c8cade2c62593af4c5
Layla Maria dos Santos Couto <layla.couto@basis.com.br>
2022-10-11 11:13:11 -0300

    ajustes estilização

--- Web/src/app/components/sidemenu/sidemenu.component.ts
+++ Web/src/app/components/sidemenu/sidemenu.component.ts
@@ -11,22 +11,26 @@ import {Router} from "@angular/router";
 export class SidemenuComponent {
     @Input() public configuracaoMenuLateral?: SidemenuModel;
 
-    @Input() public opcoes: OpcaoMenuModel[] = [
+    @Input() public opcoesControleAverco: OpcaoMenuModel[] = [
         new OpcaoMenuModel('pi pi-home', 'Início', 'Início',
             () => this.router.navigateByUrl('/Filmes')),
-        new OpcaoMenuModel('pi pi-server', 'Atores', 'Atores',
+        new OpcaoMenuModel('bi bi-file-earmark-person', 'Atores', 'Atores',
             () => this.router.navigateByUrl('/Atores')),
-        new OpcaoMenuModel('pi pi-server', 'Classes', 'Classes',
+        new OpcaoMenuModel('bi bi-ticket-detailed', 'Classes', 'Classes',
             () => this.router.navigateByUrl('/Classes')),
-        new OpcaoMenuModel('pi pi-server', 'Diretores', 'Diretores',
+        new OpcaoMenuModel('bi bi-person-video3', 'Diretores', 'Diretores',
             () => this.router.navigateByUrl('/Diretores')),
-        new OpcaoMenuModel('pi pi-server', 'Título Filmes', 'Título Filmes',
+        new OpcaoMenuModel('bi bi-film', 'Filmes', 'Título Filmes',
             () => this.router.navigateByUrl('/Filmes')),
-        new OpcaoMenuModel('pi pi-server', 'Cadastrar Itens', 'Cadastrar Itens',
-            () => this.router.navigateByUrl('/Filmes')),
-        new OpcaoMenuModel('pi pi-server', 'Cadastrar Clientes', 'Cadastrar Itens',
+        new OpcaoMenuModel('bi bi-upc-scan', 'Itens', 'Itens',
+            () => this.router.navigateByUrl('/Itens')),
+    ];
+
+    @Input() public opcoesAtendimentoCliente: OpcaoMenuModel[] = [
+        new OpcaoMenuModel('bi bi-person-plus', 'Cadastrar Clientes', 'Cadastrar Itens',
             () => this.router.navigateByUrl('/Filmes')),
     ];
+
 
     constructor(
         private router: Router
